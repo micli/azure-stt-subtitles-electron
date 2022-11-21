63d0fa137d45381b8c04b45a2555372263f76821
Michael Li <mic_lee2000@hotmail.com>
2022-11-21 22:26:51 +0800

    settings

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,16 @@
-function getSettings() { return {
-  autoPunctuation: true,
-  autoShutoffTimeMinutes: 30,
-  backgroundColor: '',
-  clearTimeSeconds: '4',
-  maxWords: '150',
-  profanityFilter: true,
-  azureKey: '',
-  azureRegion: '',
-  sourceLang: 'en-US',
-  customStlye: '',
-  blacklistWords: ''
-}}
+function getSettings() {
+    return {
+        autoPunctuation: true,
+        autoShutoffTimeMinutes: 30,
+        backgroundColor: 'transparent',
+        clearTimeSeconds: '4',
+        maxWords: '150',
+        profanityFilter: true,
+        azureKey: '',
+        azureRegion: '',
+        sourceLang: 'zh-CN',
+        destLang: 'zh-CN',
+        customStyle: '#FEFEFE',
+        blacklistWords: ''
+    }
+}
